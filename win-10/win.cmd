d6c35fbb59d09b9ee67e43445253cde95c840ec0
Josué Juca <josuejuca19@gmail.com>
2023-01-17 10:21:31 -0300

    Create win.cmd

---
+++ win-10/win.cmd
@@ -0,0 +1,21 @@
+cscript slmgr.vbs /ipk "Espaço onde ira colocar o serial do win10"
+
+cscript slmgr.vbs /skms kms.lotro.cc
+
+cscript slmgr.vbs /ato
+[--------------------------------------------------------------------------------------------------------------------
+Home/Core                                     TX9XD-98N7V-6WMQ6-BX7FG-H8Q99     
+Home/Core (Country Specific)    PVMJN-6DFY6-9CCP6-7BKTT-D3WVR
+Home/Core (Single Language)   7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH            
+Home/Core N                                3KHY7-WNT83-DGQKR-F7HPR-844BM  
+Professional(Wind 10 pro)          W269N-WFGWX-YVC9B-4J6C9-T83GX                                                                              
+Professional N(Wind 10 pro N)  MH37W-N47XK-V7XM9-C7227-GCQG9
+Enterprise                                        NPPR9-FWDCX-D2C8J-H872K-2YT43
+Enterprise N                                   DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4
+Education                                      NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
+Education N                                2WH4N-8QGBV-H22JP-CT43Q-MDWWJ
+Enterprise 2015 LTSB                   WNMTR-4C88C-JK8YV-HQ7T2-76DF9
+Enterprise 2015 LTSB N                  2F77B-TNFGY-69QQF-B8YKP-D69TJ
+Enterprise 2016 LTSB                 DCPHK-NFMTC-H88MJ-PFHPY-QJ4BJ 
+Enterprise 2016 LTSB N               QFFDN-GRT3P-VKWWX-X7T3R-8B639
+-------------------------------------------------------------------------------------------------------------------]
